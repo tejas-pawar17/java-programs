56a0ddb57d4c95c3a35a93e3e7c50d76cacc2620
Tejas Pawar <tp412102@gmail.com>
2026-01-29 19:51:38 +0530

    Arithmetic Operators

---
+++ ArithmeticOp.java
@@ -0,0 +1,34 @@
+class ArithmeticOp{
+
+	public static void main (String[]args){
+	
+	System.out.println("+ Operator");
+	System.out.println(1+"H");
+	System.out.println(1+'a');
+	System.out.println(10 + 60);
+	System.out.println("==========");
+
+	System.out.println("- Operator");
+//CTE	System.out.println(1-"a");		
+	System.out.println(10 - 'a');
+	System.out.println(10 - 60);
+	System.out.println("==========");
+
+	System.out.println("* Operator");
+	System.out.println(1*'a');
+	System.out.println(10 * 60);
+	System.out.println("==========");
+
+	System.out.println("/ Operator");
+	System.out.println(589/10);
+	System.out.println(54/10);
+	System.out.println("==========");
+
+	System.out.println("% Operator");
+	System.out.println(10 % 2);
+	System.out.println(4556 % 10);
+	System.out.println("==========");
+
+
+	}
+}
