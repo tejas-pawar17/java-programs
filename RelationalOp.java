220e6f37aec133b46f9f61426e6eb1bcc2db189b
Tejas Pawar <tp412102@gmail.com>
2026-01-30 23:10:31 +0530

    Relational Operator

---
+++ RelationalOp.java
@@ -0,0 +1,24 @@
+class RelationalOp{
+
+	public static void main(String[]args){
+
+	System.out.println(10>10);
+
+	System.out.println(10>=10);
+//	System.out.println(10 >= "10");  //CTE
+
+	System.out.println(10<9.9);
+	System.out.println(10<=10);
+
+//	System.out.println(1 > false);  //CTE
+
+	System.out.println(10==10);
+	System.out.println(10==9);
+	System.out.println(10==10.00);
+//	System.out.println(1 == true);  //CTE
+	System.out.println(1=='1');
+
+	System.out.println(10!=10);
+	System.out.println(10!=11);
+	}
+}
