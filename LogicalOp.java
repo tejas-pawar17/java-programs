b5a51c7e3c20f17cdff885e9e8dbfe153ba5c52f
Tejas Pawar <tp412102@gmail.com>
2026-01-31 12:56:32 +0530

    Logical Operator

---
+++ LogicalOp.java
@@ -0,0 +1,19 @@
+class LogicalOp{
+
+	public static void main(String[]args){
+
+	System.out.println(true && true);  //AND &&
+	System.out.println(true && false);
+	System.out.println(false && true);
+	System.out.println(false && false);
+
+	System.out.println(true || true);  //OR ||
+	System.out.println(true || false);
+	System.out.println(false || true);
+	System.out.println(false || false);
+
+	System.out.println(!true);  //NOT !
+	System.out.println(!false);
+
+	}
+}
